50940a698a40b117fd2f9317265490b5bfd169b3
cozy code <cozy.code@outlook.com>
2015-10-14 23:39:58 +0900

    add comment

--- ChimaLib/Models/SortFieldDefinition.cs
+++ ChimaLib/Models/SortFieldDefinition.cs
@@ -11,6 +11,10 @@ namespace ChimaLib.Models
     {
         private Expression<Func<TModel, TKey>> KeySelector { get; set; }
 
+        /// <summary>
+        /// Objectのフィールドに対するソートを定義
+        /// </summary>
+        /// <param name="aKeySelector">要素からキーを抽出する関数(ラムダ式)</param>
         public SortFieldDefinition(Expression<Func<TModel, TKey>> aKeySelector){
             this.SortKey = ((MemberExpression)aKeySelector.Body).Member.Name;
             this.KeySelector = aKeySelector;
@@ -18,6 +22,11 @@ namespace ChimaLib.Models
         public string SortKey{ get;  private set; }
 
         private const string DESC_SUFFIX = " desc";
+        /// <summary>
+        /// UI上で選択した値によって、次に使うべきソートキーを決定
+        /// </summary>
+        /// <param name="aCurrentSortKey">ユーザー選択値</param>
+        /// <returns></returns>
         public string GetNextSortKey(string aCurrentSortKey) {
             if (aCurrentSortKey == this.SortKey) {
                 return this.SortKey + DESC_SUFFIX;
@@ -26,6 +35,12 @@ namespace ChimaLib.Models
             
         }
 
+        /// <summary>
+        /// ソートキーとユーザー選択値を元に、クエリ(IQueryable)にOrderByを追加します。
+        /// </summary>
+        /// <param name="aQuery">クエリ</param>
+        /// <param name="aCurrentSortKey">ユーザー選択値</param>
+        /// <returns></returns>
         public IQueryable<TModel> AddOrderBy(IQueryable<TModel> aQuery, string aCurrentSortKey) {
             if (aCurrentSortKey == this.SortKey) {
                 return aQuery.OrderBy(this.KeySelector);
